4df845fb5965d6ea24f5a03339960d743ed04efa
dani <llollillu@hotmail.com>
2024-01-12 08:06:30 -0500

    he actualizado la base de datos

--- usuarios.sql
+++ usuarios.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-01-2024 a las 22:00:32
+-- Tiempo de generación: 12-01-2024 a las 14:05:24
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.1.17
 
